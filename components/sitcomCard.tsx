75fb3853d510c3c1f5693d5562e869d503a6c93a
Anirudh <madasanirudh0@gmail.com>
2026-09-17 23:59:45 +0530

    added states for selecting sitcoms

--- components/sitcomCard.tsx
+++ components/sitcomCard.tsx
@@ -1,46 +1,66 @@
+import { useState } from "react";
 import { Pressable, StyleSheet, Text, View } from "react-native";
 
-export default function SitcomCard({ title, emoji, description}: { title: string; emoji: string; description: string }) {
-  return (
-    <View style={styles.card}>
-      <Text style={styles.title}>{title}</Text>
-      <Text style={styles.emoji}>{emoji}</Text>
-      <Text style={styles.description}>{description}</Text>
 
-      <Pressable style={styles.button}>
-        <Text style={styles.buttonText}>Explore →</Text>
-      </Pressable>
-    </View>
-  );
+export default function SitcomCard({ title, emoji, description }: { title: string; emoji: string; description: string }) {
+
+    const [selected, setSelected] = useState(false);
+    function handlePress() {
+        setSelected(!selected);
+    }
+
+    return (
+        <View style={[
+            styles.card,
+            selected && styles.selectedCard,
+        ]}>
+            <Text style={styles.title}>{title}</Text>
+            <Text style={styles.emoji}>{emoji}</Text>
+            <Text style={styles.description}>{description}</Text>
+
+            <Pressable
+                style={styles.button}
+                onPress={handlePress}
+            >
+                <Text style={styles.buttonText}>
+                    {selected ? "Selected ✓" : "Explore →"}
+                </Text>
+            </Pressable>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  card: {
-    padding: 20,
-    marginVertical: 10,
-    borderRadius: 20,
-    backgroundColor: "#eee",
-  },
+    card: {
+        padding: 20,
+        marginVertical: 10,
+        borderRadius: 20,
+        backgroundColor: "#eee",
+    },
 
-  title: {
-    fontSize: 22,
-    fontWeight: "bold",
-  },
+    title: {
+        fontSize: 22,
+        fontWeight: "bold",
+    },
 
-  button: {
-    marginTop: 10,
-    padding: 10,
-  },
+    button: {
+        marginTop: 10,
+        padding: 10,
+    },
 
-  buttonText: {
-    fontSize: 16,
-  },
-  emoji: {
-    fontSize: 40,
-    marginVertical: 10, 
-  },
-  description: {
-    fontSize: 16,
-    textAlign: "center",
-  },
+    buttonText: {
+        fontSize: 16,
+    },
+    emoji: {
+        fontSize: 40,
+        marginVertical: 10,
+    },
+    description: {
+        fontSize: 16,
+        textAlign: "center",
+    },
+    selectedCard: {
+        borderWidth: 2,
+        borderColor: "#000",
+    },
 });
